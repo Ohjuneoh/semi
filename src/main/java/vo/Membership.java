5e0d57106c2425b765916aa94d40f7cc38a9db7c
mushroom3star <mushroom3star@192.168.50.135>
2023-06-08 15:03:39 +0900

    vo.membership, vo.mymembership 추가

---
+++ src/main/java/vo/Membership.java
@@ -0,0 +1,108 @@
+package vo;
+
+public class Membership {
+
+	private int no;
+	private String cat;
+	private String type;
+	private String name;
+	private String description;
+	private int price;
+	private int discountedRate;
+	private int numOfUseDay;
+	private int numOfUseWeek;
+	private int numOfPause;
+	private Gym gym;
+	
+	public Membership() {}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getCat() {
+		return cat;
+	}
+
+	public void setCat(String cat) {
+		this.cat = cat;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getDiscountedRate() {
+		return discountedRate;
+	}
+
+	public void setDiscountedRate(int discountedRate) {
+		this.discountedRate = discountedRate;
+	}
+
+	public int getNumOfUseDay() {
+		return numOfUseDay;
+	}
+
+	public void setNumOfUseDay(int numOfUseDay) {
+		this.numOfUseDay = numOfUseDay;
+	}
+
+	public int getNumOfUseWeek() {
+		return numOfUseWeek;
+	}
+
+	public void setNumOfUseWeek(int numOfUseWeek) {
+		this.numOfUseWeek = numOfUseWeek;
+	}
+
+	public int getNumOfPause() {
+		return numOfPause;
+	}
+
+	public void setNumOfPause(int numOfPause) {
+		this.numOfPause = numOfPause;
+	}
+
+	public Gym getGym() {
+		return gym;
+	}
+
+	public void setGym(Gym gym) {
+		this.gym = gym;
+	}
+	
+	
+}
