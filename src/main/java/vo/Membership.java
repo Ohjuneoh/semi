98c7fc6404c9425f27ed9498115ea2a2bca44d27
yuri <oooorrrr0117@gmail.com>
2023-06-12 11:02:53 +0900

    멤버쉽수정

--- src/main/java/vo/Membership.java
+++ src/main/java/vo/Membership.java
@@ -12,7 +12,7 @@ public class Membership {
 	private int numOfUseDay;
 	private int numOfUseWeek;
 	private int numOfPause;
-	private double duration;
+	private String duration;
 	private int count;
 	private Gym gym;
 	
@@ -27,11 +27,11 @@ public class Membership {
 	}
 
 
-	public double getDuration() {
+	public String getDuration() {
 		return duration;
 	}
 
-	public void setDuration(double duration) {
+	public void setDuration(String duration) {
 		this.duration = duration;
 	}
 
--- src/main/webapp/lesson/groupRegiseterLesson.jsp
+++ src/main/webapp/lesson/groupRegiseterLesson.jsp
@@ -11,13 +11,14 @@
 	
 	// 로직수행
 		// 정원수 <= 신청자수면 신청불가능  
-	GroupeLessonDao lessonDao = GroupeLessonDao.getinstance();
+		
+/*	GroupeLessonDao lessonDao = GroupeLessonDao.getinstance();
 	Lesson lesson = lessonDao.getGroupLessonByLessonNo(lessonNo);
 	
 	if(lesson.getQuota() <= lesson.getReqCnt()) {
 		
 	}
-	
+*/ 	
 		// 예약테이블에 저장
 	
 		// 강좌테이블에 신청자수 +1 
